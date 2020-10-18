453d214df6418b76f13ab20f6b108473c72ccdc7
Juan Pablo Tyrlik <juan.pablo.tyrlik@gmail.com>
2020-10-18 19:09:34 -0300

    Hero Card

--- src/components/heroes/HeroScreen.js
+++ src/components/heroes/HeroScreen.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Redirect, useHistory, useParams } from 'react-router-dom';
+import { getHeroById } from '../../selectors/getHeroById';
 
 export const HeroScreen = () => {
+    const { heroId } = useParams();
+    const hero = getHeroById(heroId);
+
+    const history = useHistory();
+
+    if (!hero) return <Redirect to={history.goBack()} />;
+
+    const {
+        superhero,
+        publisher,
+        alter_ego,
+        first_appearance,
+        characters,
+    } = hero;
+
     return (
         <div>
             <h1>Hero Screen</h1>
+            <h5>{heroId}</h5>
         </div>
     );
 };
--- src/routers/DashboardRouter.js
+++ src/routers/DashboardRouter.js
@@ -13,11 +13,7 @@ export const DashboardRouter = () => {
                 <Switch>
                     <Route exact path="/marvel" component={MarvelScreen} />
                     <Route exact path="/dc" component={DcScreen} />
-                    <Route
-                        exact
-                        path="/heroes/:heroId"
-                        component={HeroScreen}
-                    />
+                    <Route exact path="/hero/:heroId" component={HeroScreen} />
                     <Redirect to="/marvel" />
                 </Switch>
             </div>
